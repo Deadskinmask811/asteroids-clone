3d16a791eac5be6adcbd921b2ced66b053ffdd27
Deadskinmask811 <cstover811@gmail.com>
2025-07-04 22:23:56 -0400

    Added fps limiting

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        tick = clock.tick(60)
+        dt = tick / 1000
 
 
 if __name__ == "__main__":
